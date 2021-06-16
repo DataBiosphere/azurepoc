1293255c13312fdd72ef9501d40673150a6a0fc5
mflinn-broad <mflinn@broadinstitute.org>
2021-06-16 10:19:32 -0400

    add boilerplate

---
+++ src/test/java/bio/terra/azurepoc/AzurepocApplicationTests.java
@@ -0,0 +1,13 @@
+package bio.terra.azurepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzurepocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
